2bf07ed78fd4f6eb4259ade66680234f6ce1b316
kevcal <kevcal@channelfix.com>
2018-11-16 15:15:51 +0800

    Added precense check

--- contrib/urls.py
+++ contrib/urls.py
@@ -6,5 +6,6 @@ from contrib import views
 
 urlpatterns = [
     url(r'^service-worker.js$', views.ServiceWorker.as_view(), name='service-worker'),
+    url(r'^presence$', views.PresenceTestView.as_view(), name='presence'),
     url(r'^$', views.IndexView.as_view(), name='index'),
 ]
--- contrib/views.py
+++ contrib/views.py
@@ -1,10 +1,14 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
+import json
+import requests
 
 from django.http import HttpResponse
 from django.template import loader, Context
 from django.views.generic import TemplateView, View
 
+
+PRESENCE_API = 'https://api.pushy.me/devices/presence?api_key=some_api_key'  
 
 class IndexView(TemplateView):
     template_name = 'contrib/index.html'
@@ -17,3 +21,19 @@ class ServiceWorker(View):
         response = HttpResponse(javascript.render({}))
         response['Content-Type'] = 'application/javascript'
         return response
+
+
+class PresenceTestView(TemplateView):
+	template_name = 'contrib/presence.html'
+
+	def post(self, request, *args, **kwargs):
+		device = request.POST.get('device', 0)
+		tokens = list(map(str, [device]))
+		payload = {'registration_ids': tokens}
+		req = requests.post(
+			PRESENCE_API,
+			headers={'Content-Type': 'application/json'},
+			data=json.dumps(payload))
+		context = self.get_context_data(*args, **kwargs)
+		context['response'] = req.text
+		return self.render_to_response(context)
